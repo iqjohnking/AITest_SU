7dcc19876e699a06311cecee4407753733a25c84
Inko_Su <iqjohnking@gmail.com>
2021-12-30 13:22:14 +0800

    ver0.62
    ver0.62

---
+++ Assets/cSharp/pool/CheckPoint.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    //"=>"Lambda 運算子
+    string Name => gameObject.name;
+    string getNum;
+
+    int num;
+    public int GetNum => num;
+    //後面num被放入
+
+    void Start()
+    {
+        for (int i = 10; i < Name.Length; i++)
+        {
+            //(string getNum) = (string getNum) + (string Name[i])
+            //也就是說命名方式是getNum後掛數字
+            getNum += Name[i];
+        }
+        //將string getNum 轉型，放入num
+        num = int.Parse(getNum);
+    }
+}
--- Assets/cSharp/pool/PurpleCubes.cs
+++ Assets/cSharp/pool/PurpleCubes.cs
@@ -6,8 +6,9 @@ public class PurpleCubes : MonoBehaviour
 {
     //checkpoint的計數器
     //List用來裝checkpoint
-    int checkPointNum = 0;
+    int PointNum = 0;
     List<string> CheckPoints;
+    Transform target;
 
     [SerializeField]
     float speed = 10f;
@@ -21,6 +22,61 @@ public class PurpleCubes : MonoBehaviour
     {
         
     }
+
+    void MoveTo(Vector3 destination)
+    {
+        //Vector3.Distance() 求長度
+        //設定死區以防呆
+        if (Vector3.Distance(transform.position, destination) < 0.1f) return;
+        //調整物件朝向，一種有Lerp一種沒有
+        Vector3.Lerp(transform.forward, (destination - transform.position).normalized, Time.deltaTime);
+        transform.forward = (destination - transform.position).normalized;
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+
+
+    //被collier碰撞到目標的時候
+    private void OnTriggerEnter(Collider other)
+    {
+        //只在這裡運作的function
+        void pathRun(List<string> checks, int key)
+        {
+            if (key > (checks.Count - 1))
+            {
+                return;
+            }
+            else
+            {
+                if (other.name == checks[key])
+                {
+                    if (key + 1 > (checks.Count - 1))
+                    {
+                        target = GameObject.Find("Sphere").transform;
+                        return;
+                    }
+                    target = GameObject.Find(checks[key + 1]).transform;
+                }
+                pathRun(CheckPoints, key + 1);
+            }
+        }
+        pathRun(CheckPoints, 0);
+        if (other.name == "Sphere")
+        {
+            if (CheckPoints.Count > 0)
+            {
+                target = GameObject.Find(CheckPoints[0]).transform;
+            }
+            else
+            {
+                target = GameObject.Find("Sphere").transform;
+            }
+        }
+    }
+
+
+
 
 
 
